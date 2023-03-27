8df2af8328ab93f557157c17f77d172e4a744810
Clive Lai <93177532+clivelai@users.noreply.github.com>
2023-03-28 00:22:58 +0800

    Update main.cpp

--- main/main.cpp
+++ main/main.cpp
@@ -2,6 +2,39 @@
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 
-int main() {
+int main() 
+{
+	// GLFW initialize
+	glfwInit();
+
+	// GLFW hint: get certain hint by passing value
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	/************* Handling GLFW window *************/
+	// Initialize GLFW window
+	GLFWwindow* window = glfwCreateWindow(800, 800, "Example Screen", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	};
+	// Create GLFW window
+	glfwMakeContextCurrent(window);
+	// Close GLFW window
+	// glfwDestroyWindow(window);
+	// Handling GLFW window close behavior
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+
+	/********** End of handling GLFW window **********/
+
+	// GLFW terminate
+	glfwTerminate();
+
 	return 0;
 }
